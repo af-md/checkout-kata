9182dd272b49ba2e52297e3ea5b10a321cff1c0f
Afzal M <afzaalm059@gmail.com>
2024-05-06 12:05:05 +0100

    get total price with multiple discounts on multiple items

--- pricingscheme/pricingscheme.go
+++ pricingscheme/pricingscheme.go
@@ -5,3 +5,18 @@ type PricingScheme struct {
 	Quantity        int
 	DiscountedPrice int
 }
+
+func GetPricingScheme() []PricingScheme {
+	// this function is used to get a fresh pricing scheme from the model
+	// can be replaced to a call to a database or an API
+
+	return []PricingScheme{{
+		Sku:             "A",
+		Quantity:        3,
+		DiscountedPrice: 12,
+	}, {
+		Sku:             "B",
+		Quantity:        3,
+		DiscountedPrice: 22,
+	}}
+}
